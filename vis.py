7a75cd648c7258028833ef4b0e0f9584f5fb19cc
James <jhagerty@alumni.cmu.edu>
2023-02-15 21:54:37 -0500

    top brain

--- vis.py
+++ vis.py
@@ -1,5 +1,6 @@
 import torchio as tio
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def main():
@@ -14,16 +15,24 @@ def main():
     t1ce_img = tio.ScalarImage(t1ce_path)
     t2_img = tio.ScalarImage(t2_path)
     seg_img = tio.LabelMap(seg_path)
-    
+
+    # img_2d_stack default is sideways brain view where:
+    # image top is brain front, image bottom is brain back, image left is brain
+    # bottom, image right is brain top
+    # img_2d_stack.shape (240, 240, 155), arbitrarily treat it as (z, y, x)
+    img_2d_stack = flair_img.numpy()[0] # batch i
+
+    top_brain_stack = np.swapaxes(img_2d_stack, 0, 2) # (x, y, z); (155, 240, 240)
+    for i in range(40, 60):
+        plt.imsave(f"out/{i}.png", top_brain_stack[i])
+
+
     # fig, axs = plt.subplots(nrows=2, ncols=2)
     # print(flair_img.shape)
-    # axs[0][0].imshow(flair_img.numpy()[0][10])
+    # axs[0][0].imshow( numpy arr )
     # plt.savefig("test.png")
 
-    print(flair_img.shape)
-    # (1, 240, 240, 155)
-    for i in range(100):
-        plt.imsave(f"out/{i}.png", flair_img.numpy()[0][i])
+    # also try plt for 3d data!
 
 if __name__ == "__main__":
     main()
