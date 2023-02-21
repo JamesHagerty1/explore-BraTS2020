04112adec7571f3440ab1e4baef50dc672148728
James <jhagerty@alumni.cmu.edu>
2023-02-21 18:52:20 -0500

    enc skeleton

--- gen_config.py
+++ gen_config.py
@@ -11,6 +11,10 @@ BATCH_SIZE = 2
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 PIN_MEMORY = True if DEVICE == "cuda" else False
 
+ENC_CHANNELS = ((1, 32, 64), (64, 64, 128), (128, 128, 256), (256, 256, 512))
+CONV_KERNEL_SIZE = 3
+POOL_KERNEL_SIZE = 2
+
 
 ################################################################################
 
@@ -32,10 +36,12 @@ def main():
         "batch_size": BATCH_SIZE,
         "device": DEVICE,
         "pin_memory": PIN_MEMORY,
+        "enc_channels": ENC_CHANNELS,
+        "conv_kernel_size": CONV_KERNEL_SIZE,
+        "pool_kernel_size": POOL_KERNEL_SIZE,
     }
     with open("config.json", "w") as json_file:
         json.dump(json_data, json_file, indent=2)
 
 if __name__ == "__main__":
     main()
-    
--- model.py
+++ model.py
@@ -8,8 +8,37 @@ import torch.nn as nn
 class UNet3D(nn.Module):
 	def __init__(self, c):
 		super().__init__()
+		self.encoder = Encoder(c.enc_channels, c.conv_kernel_size,
+			c.pool_kernel_size)
 		
 	def forward(self, x):
-		print(x.shape)
+		enc_features = self.encoder(x)
+		return x
+	
+
+class Encoder(nn.Module):
+	def __init__(self, channels, conv_kernel_size, pool_kernel_size):
+		super().__init__()
+		self.conv_blocks = nn.ModuleList(
+			[ConvBlock(channels[i], conv_kernel_size) 
+                for i in range(len(channels))])
+		self.pool = nn.MaxPool3d(pool_kernel_size)
+
+	def forward(self, x):
+		return x
+
+
+class ConvBlock(nn.Module):
+	def __init__(self, channels, conv_kernel_size):
+		super().__init__()
+		print("hii")
+		# self.conv1 = nn.Conv3d(in_channels, out_channels, kernel_size)
+		# self.relu = nn.ReLU()
+		# self.conv2 = nn.Conv3d(out_channels, out_channels, kernel_size)
+		
+	def forward(self, x):
+		# x = self.conv1(x)
+		# x = self.relu(x)
+		# x = self.conv2(x)
 		return x
 	
