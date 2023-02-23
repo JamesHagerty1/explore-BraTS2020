ef8b86d945cffa2cb1386392d00cc12392e5a58e
James <jhagerty@alumni.cmu.edu>
2023-02-22 20:47:19 -0500

    config for decoder

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 Dataset:
 https://www.kaggle.com/datasets/awsaf49/brats20-dataset-training-validation/code
-Unet ref (also visualizes this dataset):
+
+Ref for some visuals:
 https://www.kaggle.com/code/tuankiennguyen/brain-segmentation-with-pytorch
 
-explore data with similar 3D unet https://arxiv.org/pdf/1606.06650.pdf
-
-implementation is a 3D version of https://pyimagesearch.com/2021/11/08/u-net-training-image-segmentation-models-in-pytorch/
+Explored data with 3D unet, architecture from:
+https://arxiv.org/pdf/1606.06650.pdf
--- gen_config.py
+++ gen_config.py
@@ -1,4 +1,5 @@
 import json
+import os
 import torch
 
 
@@ -6,12 +7,17 @@ import torch
 
 
 TRAIN_DIR = "MICCAI_BraTS2020_TrainingData"
+NUM_LABELS = -1
+
 NUM_SAMPLES = 4
 BATCH_SIZE = 2
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 PIN_MEMORY = True if DEVICE == "cuda" else False
 
-ENC_CHANNELS = ((1, 32, 64), (64, 64, 128), (128, 128, 256), (256, 256, 512))
+ENC_CONV_CHANNELS = ((1, 32, 64), (64, 64, 128), (128, 128, 256), (256, 256, 512))
+DEC_UPCONV_CHANNELS = ((512, 512), (256, 256), (128, 128))
+# excluding decoder conv for inference, which is inferred by NUM_LABELS
+DEC_CONV_CHANNELS = ((256+512, 256, 256), (128+256, 128, 128), (64+128, 64, 64))
 CONV_KERNEL_SIZE = 3
 POOL_KERNEL_SIZE = 2
 
@@ -30,17 +36,22 @@ class AttrDict(dict):
 
 
 def main():
+    if os.path.exists("./config.json"):
+        os.remove("./config.json")
     json_data = {
         "train_dir": TRAIN_DIR,
+        "num_labels": NUM_LABELS,
         "num_samples": NUM_SAMPLES,
         "batch_size": BATCH_SIZE,
         "device": DEVICE,
         "pin_memory": PIN_MEMORY,
-        "enc_channels": ENC_CHANNELS,
+        "enc_conv_channels": ENC_CONV_CHANNELS,
+        "dec_upconv_channels": DEC_UPCONV_CHANNELS,
+        "dec_conv_channels": DEC_CONV_CHANNELS,
         "conv_kernel_size": CONV_KERNEL_SIZE,
         "pool_kernel_size": POOL_KERNEL_SIZE,
     }
-    with open("config.json", "w") as json_file:
+    with open("./config.json", "w") as json_file:
         json.dump(json_data, json_file, indent=2)
 
 if __name__ == "__main__":
